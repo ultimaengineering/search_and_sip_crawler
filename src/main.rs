0c0d9bc36a3b41e3f843ed802a904928e7426d2e
Alexander Montgomery <evaderxander@gmail.com>
2020-11-05 01:01:31 -0500

    getting domains listed. Going to focus on a breathsfirst search

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,25 @@
+use select::document::Document;
+use select::predicate::Name;
+use reqwest::Url;
+
 fn main() {
-    println!("Hello, world!");
+        get_page();
 }
 
-fn parse_website() {
 
+#[tokio::main]
+async fn get_page() -> Result<(), reqwest::Error> {
+    let res = reqwest::get("https://news.ycombinator.com/").await?;
+   // println!("Body:\n\n{}", res.text().await?);
+    let x =  res.text().await?;
+    get_new_links(x);
+    Ok(())
+}
+
+fn get_new_links(html: String) {
+    let document = Document::from(&*html).find(Name("a"))
+        .filter_map(|n| n.attr("href"))
+        .filter(|n| n.contains("http"))
+        .map(|u| Url::parse(u))
+        .for_each(|x| println!("{:?}", x));
 }
